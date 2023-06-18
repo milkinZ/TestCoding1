1a8f19752a73a0354b7a0e262b6e93bc86db2dd8
milkinZ <ardhanurfajri157@gmail.com>
2023-06-18 17:38:44 +0700

    Test Coding

---
+++ .next/static/PCVJKS0lr84XMwe-sefZt/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-6b5af3850234e1eb.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
